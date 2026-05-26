b36de194be39d8b820a73e1a891e941f2311f14d
TimewasterKayla <timewasterkayla@gmail.com>
2026-05-26 01:58:51 -0700

    Beta 5.2

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default function HomePage() {
     }
 
     // ---------------------------------
-    // SUCCESS
+    // SUCCESSS
     // ---------------------------------
     window.location.href = '/games'
   }
